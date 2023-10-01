82c04d6d4b70285d76bbcda8402d7612e039eaa4
tho.nguyen <91511523+haki203@users.noreply.github.com>
2023-10-01 09:47:52 +0700

    .

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, LogBox, Image, } from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black'
+  },
+});
+
+export default App;
