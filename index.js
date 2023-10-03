071ed52210f235d43a743e65f8c51300677cd526
tuananh2k321 <haizzj123@gmail.com>
2023-10-03 18:22:55 +0700

    TA done task fetch axios

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import List from './tuan_anh/List';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => List);
