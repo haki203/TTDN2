18d8cac643d1ff4642ce9822736be4ba80590896
tuananh2k321 <haizzj123@gmail.com>
2023-10-03 17:40:24 +0700

    TA update code

---
+++ List.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+
+const List = () => {
+    return(
+        <SafeAreaView>
+            <Text
+                style={{
+                    
+                }}
+            >List</Text>
+        </SafeAreaView>
+    )
+}
+
+export default List
