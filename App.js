a959e3f9f15a4bbb7d8a6eb71466b1a4f103385f
ThChien123 <117609417+ThChien123@users.noreply.github.com>
2023-10-06 12:21:41 +0700

    chien
    Hook

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, LogBox, Image, } from 'react-native';
+/* eslint-disable prettier/prettier */
+
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View, LogBox, Image} from 'react-native';
 import Screen1 from './src/animation/Screen1';
 import GoogleLogin from './src/googleLogin/googleLogin';
-
+import Counter from './src/hooks/Counter';
+import {Provider} from './src/hooks/CounterContext';
 
 const App = () => {
-
   return (
-    <GoogleLogin/>
+    <GoogleLogin />
+    //Hook ---->
+    //   <Provider>
+    //   <View style={styles.container}>
+    //     <Counter />
+    //   </View>
+    // </Provider>
+    // <----Hook
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black'
+    backgroundColor: 'black',
+  },
+  container1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
