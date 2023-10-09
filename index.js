19225a3c7dc7a43efceba36e2ff7a83c5de84dfc
theking250902 <buivanvietx5hongloc@gmail.com>
2023-10-09 23:29:31 +0700

    viet

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import HomeScreen from './src/components/SettingScreen';
 
-AppRegistry.registerComponent(appName, () => App);
+
+HomeScreen
+
+AppRegistry.registerComponent(appName, () => HomeScreen);
