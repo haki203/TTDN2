a3116dd5d21fb35d5002af3ea2ee13877571dee6
ThChien123 <117609417+ThChien123@users.noreply.github.com>
2023-10-11 20:13:33 +0700

    chien

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, LogBox, Image, } from 'react-native';
-import { Provider } from 'react-redux';
+/* eslint-disable prettier/prettier */
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View, LogBox, Image} from 'react-native';
+import {Provider} from 'react-redux';
 import store from './store/store';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import AppNavigator from './navigation/AppNavigator';
-
-
+import WaitScreen from './components/WaitScreen';
+import Welcome from './components/Welcome';
+import Read from './components/Read';
 const App = () => {
-
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </Provider>
-
+    // <Provider store={store}>
+    //   <NavigationContainer>
+    //     <AppNavigator />
+    //   </NavigationContainer>
+    // </Provider>
+    <View style={styles.container}>
+      <Read />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black'
+    // backgroundColor: 'black',
   },
 });
 
---
+++ src/components/ScreenWrapper.js
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+const ScreenWrapper = ({ children }) => {
+  return(
+    <LinearGradient style={styles.container} colors={['#6fa8dc', '#cfe2f3']}>
+        {children}
+    </LinearGradient>
+  )
+   
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default ScreenWrapper;
