f9e120cbffea68a4cbad515bb04b18475d62731f
tho.nguyen <91511523+haki203@users.noreply.github.com>
2023-11-07 19:54:25 +0700

    Update FavouriteModel.js

--- server/components/products/FavouriteModel.js
+++ server/components/products/FavouriteModel.js
@@ -6,7 +6,7 @@ const moment = require('moment'); // Import thư viện moment
 const schema = new Schema({
     bookId: { type: ObjectId, ref: 'books' },
     userId: { type: ObjectId, ref: 'users' },
-    date: { type: Date,default:moment().format('DD/MM/YYYY')},
+    date: { type: String,default:moment().format('DD/MM/YYYY')},
 });
 // Đảm bảo rằng bảng Products chưa được định nghĩa trước đó
 module.exports = mongoose.model('favorite', schema, 'favorites');
